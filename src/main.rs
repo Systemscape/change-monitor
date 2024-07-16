4d7ca8352fd0b805bbe774bad1ad3fdc975ce1a9
Joël Schulz-Andres <joel@systemscape.de>
2024-07-16 18:53:18 +0200

    Add --date flag

--- src/main.rs
+++ src/main.rs
@@ -46,11 +46,12 @@ fn check_git_repository() -> Result<ExitStatus, String> {
 }
 
 /// Finds the latest
-fn get_latest_commit(files: Vec<&str>) -> Option<String> {
+fn get_latest_commit(files: Vec<&str>, get_date: bool) -> Option<String> {
+    let format = if get_date { "--pretty=format:%cs" } else { "--pretty=format:%H" };
     let output = Command::new("git")
         .arg("log")
         .arg("-1")
-        .arg("--pretty=format:%H")
+        .arg(format)
         .args(&["--"])
         .args(files)
         .output()
@@ -73,7 +74,7 @@ fn get_latest_commit(files: Vec<&str>) -> Option<String> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
+    if args.len() < 2 || args.len() > 3 {
         eprintln!("Usage: {} <filename>", args[0]);
         std::process::exit(1);
     }
@@ -81,6 +82,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     check_git_repository()?;
 
     let filename = &args[1];
+    let get_date = args.get(2).map_or(false, |arg| arg == "--date");
+
     let dependencies_path = Path::new(".deps.toml");
 
     // If there is no .deps.toml file, just use the local folder
@@ -105,17 +108,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         files
     };
 
-    let latest_commit = get_latest_commit(all_files.clone()).filter(|s| !s.is_empty());
+    let latest_commit = get_latest_commit(all_files.clone(), get_date).filter(|s| !s.is_empty());
 
     if let Some(commit_hash) = latest_commit {
         //println!("Latest commit affecting {}: {}", filename, commit_hash);
         let mut owned_hash: String = commit_hash.to_owned();
-        let owned_flag: String = check_clean_working_tree(&all_files).to_owned();
-        owned_hash.push_str(&owned_flag);
+        if !get_date {
+            let owned_flag: String = check_clean_working_tree(&all_files).to_owned();
+            owned_hash.push_str(&owned_flag);
+        }
         println!("{owned_hash}");
     } else {
         eprintln!("No commits found.");
         std::process::exit(1);
     }
+
     Ok(())
 }
